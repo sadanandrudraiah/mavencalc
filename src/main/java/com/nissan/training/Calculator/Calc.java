22303987b3839df085e558b666a328ef8e72d079
sadanandrudraiah <sadanandrudraiah@users.noreply.github.com>
2018-08-17 12:53:07 +0530

    Update Calc.java

--- src/main/java/com/nissan/training/Calculator/Calc.java
+++ src/main/java/com/nissan/training/Calculator/Calc.java
@@ -3,11 +3,13 @@ package com.nissan.training.Calculator;
 public class Calc {
 public int add(int a,int b)
 {
+	int x = 10;
 	return (a+b);
 }
 
 public int sub(int a,int b)
 {
+	int y =0;
 	return (a-b);
 }
 }
